ed8f3f81286e9705b5ff4258b26a7784af97c1f8
Pickachu Chen <pickachu1006@gmail.com>
2022-02-16 11:56:31 +0800

    20220216_delete lottery

--- js/all.js
+++ js/all.js
@@ -16,15 +16,15 @@ $(document).ready(function () {
   });
 
   //週年慶活動辦法下拉選單
-  $(".anniversary_2,.SP_anniversary_2").hide();
-  $(".anniversary_1, .SP_anniversary_1").click(function (e) {
-    e.preventDefault();
-    $(".anniversary_2,.SP_anniversary_2").toggle();
-  });
-  $(".anniversary_2, .SP_anniversary_2").click(function (e) {
-    e.preventDefault();
-    $(".anniversary_2, .SP_anniversary_2").hide();
-  });
+  // $(".anniversary_2,.SP_anniversary_2").hide();
+  // $(".anniversary_1, .SP_anniversary_1").click(function (e) {
+  //   e.preventDefault();
+  //   $(".anniversary_2,.SP_anniversary_2").toggle();
+  // });
+  // $(".anniversary_2, .SP_anniversary_2").click(function (e) {
+  //   e.preventDefault();
+  //   $(".anniversary_2, .SP_anniversary_2").hide();
+  // });
 
   //偵測sns line btn 手機版還是電腦版，連結不同
   // var vw=$(window).width();
